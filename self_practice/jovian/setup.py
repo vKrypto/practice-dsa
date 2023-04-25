73453baef7064e205c53ee726b5e50a4a8b94e35
Ashutosh55 <ashutosh.verma@ornaz.com>
2023-04-25 10:25:34 +0530

    auto syncing.

--- self_practice/jovian/setup.py
+++ self_practice/jovian/setup.py
@@ -1,25 +1,50 @@
 from datetime import datetime
 import pandas as pd
-import jovian
+import json
+from pprint import pprint
 
 # Read in the invoice data as a dataframe
 df = pd.read_csv("invoice.csv")
 
+map_invoices = {}
+last_invoice_number = 0
+last_invoice_date = None
 # Loop through each row of the dataframe
 for i in range(len(df) - 1):
     try:
         current_invoice = df.iloc[i]
-        next_invoice = df.iloc[i + 1]
+
+        current_invoice_number = int(current_invoice["invoice_number"])
         current_date = datetime.strptime(current_invoice["talley_invoice_date"], "%d-%b-%Y")
-        next_date = datetime.strptime(next_invoice["talley_invoice_date"], "%d-%b-%Y")
-        
-        # Check if the invoice numbers and dates are in order
-        if current_invoice["invoice_number"] > next_invoice["invoice_number"] or \
-         current_date >= next_date:
-            # Remove the row from the dataframe
-            df = df.drop(i)
+        label = current_date.strftime("01-%m-%y")
+        if map_invoices.get(label) is None:
+            map_invoices[label] = []
+        map_invoices[label].append(current_invoice_number)
+
+
+
+        # if last_invoice_date is None:
+        #     last_invoice_date = current_date
+        # # Check if the invoice numbers and dates are in order
+        # if current_invoice_number > last_invoice_number and current_date >= last_invoice_date:
+        #     # Remove the row from the dataframe
+        #     last_invoice_number = current_invoice_number
+        #     last_invoice_date = current_date
+        #     df = df.drop(i)
+        # else:
+        #     print(current_invoice_number, last_invoice_number, current_date, last_invoice_date)
     except Exception as e:
         print(str(e))
+        break
 
 # Output the filtered dataset
 df.to_csv("filtered_invoice_data.csv", index=False)
+
+with open('data.json', 'w', encoding='utf-8') as f:
+    json.dump(map_invoices, f, ensure_ascii=False, indent=4)
+
+
+
+new_map_data = {}
+for key, obj in map_invoices.items():
+    print(key, min(obj), max(obj))
