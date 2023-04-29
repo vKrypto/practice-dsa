274da4c3629409ff0120fee64fd4a03c910c9d59
vkrypto <vkrypto@gmail.com>
2023-04-30 02:36:34 +0530

    auto syncing..

---
+++ dsa/fuck-coding-interviews/problems/__init__.py
@@ -0,0 +1,9 @@
+from typing import Optional, List
+
+
+
+class TreeNode:  # pragma: no cover
+    def __init__(self, val=0, left=None, right=None) -> Optional(List):
+        self.val = val
+        self.left = left
+        self.right = right
