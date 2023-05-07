4d6b2850f6db1d830e8608c4b550c648967e2f1d
Ashutosh55 <ashutosh.verma@ornaz.com>
2023-05-08 01:13:35 +0530

    auto syncing.

--- dsa/fuck-coding-interviews/problems/search-in-rotated-sorted-array.py
+++ dsa/fuck-coding-interviews/problems/search-in-rotated-sorted-array.py
@@ -3,10 +3,28 @@
 from typing import List
 
 class Solution:
+
+    # time = o(n), sapce = o(1)
     def search(self, nums: List[int], target: int) -> int:
-        
-        l, r = 0, len(nums)-1
-        while l <= r:
-            m = (l+r)//2
-            if target < nums[m]:
-                r = m-1
+        left, right = 0, len(nums)-1
+
+        while left <= right:
+            mid = (left+right)//2
+            # base-case
+            if target == nums[mid]:
+                return mid
+            
+            # searching in left part
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target <= nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            # searching in right part 
+            else:
+                if nums[mid] <= target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid-1
+                
+        return -1
