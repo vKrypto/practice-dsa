4b14d9d0f6bef3bb32b71587f464f4060790b66f
Ashutosh55 <ashutosh.verma@ornaz.com>
2023-05-28 23:58:59 +0530

    auto syncing with msg-''

--- dsa/fuck-coding-interviews/problems/word-Search.py
+++ dsa/fuck-coding-interviews/problems/word-Search.py
@@ -7,46 +7,12 @@ The word can be constructed from letters of sequentially adjacent cells, where a
 Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
 Output: true
 '''
-from . import List
 from itertools import product, starmap
-
-
-# Solution
-def exist(board, word):
-    ROWS, COLS = len(board), len(board[0])
-    path = set()
-
-    def dfs(r, c, i):
-        if i == len(word):
-            return True
-        if (r < 0 or c < 0 or
-            r >= ROWS or c >= COLS or
-            word[i] != board[r][c] or
-                (r, c) in path):
-            return False
-
-        path.add((r, c))
-        res = (dfs(r+1, c, i+1) or
-               dfs(r-1, c, i+1) or
-               dfs(r, c+1, i+1) or
-               dfs(r, c-1, i+1))
-        path.remove((r, c))
-        return res
-
-    for r in range(ROWS):
-        for c in range(COLS):
-            if dfs(r, c, 0):
-                return True
-    return False
-
-#T: O(n*m*4^n)
-#S: O(n)
-
-
+from . import List
 
 
 class Solution:
-    # O(n * m * 4^n)
+    # time: O(n * m * 4^n). space: O(n)
     def exist(self, board: List[List[str]], word: str) -> bool:
         rows = len(board)
         cols = len(board[0])
---
+++ dsa/fuck-coding-interviews/problems/word-search-ii.py
@@ -0,0 +1,42 @@
+from itertools import product, starmap
+from . import List
+
+class Solution:
+    
+    # time: O(n * m * 4^n). space: O(n)
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        n, m, wrdlen = len(board), len(board[0]), len(word)
+        if wrdlen > m * n:
+            return False
+
+        def dfs(r, c, i=0) -> bool:
+            if board[r][c] != word[i]:
+                return False
+            if i == wrdlen - 1:
+                return True
+            
+            board[r][c] = "#"
+            i += 1
+            valid = (
+                (r > 0 and dfs(r - 1, c, i))
+                or (r < n - 1 and dfs(r + 1, c, i))
+                or (c > 0 and dfs(r, c - 1, i))
+                or (c < m - 1 and dfs(r, c + 1, i))
+            )
+            board[r][c] = word[i - 1]
+            return valid
+
+        cnts = [0] * 58
+        for r, c in product(range(n), range(m)):
+            cnts[ord(board[r][c]) - ord("A")] += 1
+        if cnts[ord(word[0]) - ord("A")] > cnts[ord(word[-1]) - ord("A")]:
+            word = word[::-1]
+        return any(starmap(dfs, product(range(n), range(m))))
+    
+    
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        output = []
+        for word in words:
+            if self.exist(board, word):
+                output.append(word)
+        return output
