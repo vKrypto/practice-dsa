d1c05ad19177ca78defc84c09aada6698ab7e68f
Ashutosh55 <vkrypto@gmail.com>
2023-05-31 01:44:00 +0530

    auto syncing with msg-''

--- dsa/fuck-coding-interviews/problems/kth-largest-element-in-an-array.py
+++ dsa/fuck-coding-interviews/problems/kth-largest-element-in-an-array.py
@@ -7,19 +7,25 @@ import heapq
 
 
 class Solution:
+    # time: o(nlogn), space: o(1)
     def findKthLargest(self, nums: List[int], k: int) -> int:
         sorted_nums = sorted(nums)
         return sorted_nums[len(sorted_nums) - k]
 
 
 class Solution2:
+    # time= o((n+k)logn) # space: o(n)
     def findKthLargest(self, nums: List[int], k: int) -> int:
         max_heap = []
+        # o(n)
         for num in nums:
+            # o(log(n))
             heapq.heappush(max_heap, -num)
 
         kth_largest = None
+        # o(k)
         for _ in range(k):
+            # o(log(n))))
             kth_largest = heapq.heappop(max_heap)
         return -kth_largest
 
---
+++ dsa/fuck-coding-interviews/problems/last-stone-weight.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/last-stone-weight/description/
+import heapq
+from . import List
+
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        if len(stones)==1:
+            return stones[0]
+        stone_heap = [-i for i in stones]
+        heapq.heapify(stone_heap)
+
+        while len(stone_heap) > 1:
+            first_max = heapq.heappop(stone_heap) # -3
+            second_max = heapq.heappop(stone_heap) # -2
+            if first_max != second_max:
+                heapq.heappush(stone_heap, (first_max-second_max))
+
+        return -stone_heap[0] if stone_heap else 0
+
+    def lastStoneWeight_1(self, stones):
+        stones.sort()
+        while(len(stones)>1):
+            stone1 = stones.pop()
+            stone2 = stones.pop()
+            stones.append(stone1-stone2)
+            stones.sort()
+        return stones[0]
