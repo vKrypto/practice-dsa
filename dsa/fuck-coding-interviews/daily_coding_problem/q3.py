cbe526078c97f0f4efde691986f59720e567cec6
vkrypto <vkrypto@gmail.com>
2023-06-01 23:34:48 +0530

    syncing to repo..

--- dsa/fuck-coding-interviews/daily_coding_problem/q3.py
+++
@@ -1,9 +0,0 @@
-# A and B are 2 arrays of numbers (they can be unequal length, min len=1). 
-
-# Their sums are unequal.
-
-# However just by one swap in these 2 arrays their sum will become equal.
-
-# You have to return the 2 elements which when swapped, make the sum of A and B equal. 
-
-# Can you solve this in 0(n) time ?
--- dsa/fuck-coding-interviews/problems/big_countries.sql
+++
@@ -1,7 +0,0 @@
--- https://leetcode.com/problems/big-countries/
-
-SELECT name, population, area
-FROM World
-WHERE
-    area > 3000000 OR
-    population > 25000000;
