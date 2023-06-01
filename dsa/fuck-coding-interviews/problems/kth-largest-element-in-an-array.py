4d39f3078a81c095cc4efca6806c8756d39240dd
vkrypto <vkrypto@gmail.com>
2023-06-02 00:07:52 +0530

    syncing to repo..

--- dsa/fuck-coding-interviews/problems/kth-largest-element-in-an-array.py
+++ dsa/fuck-coding-interviews/problems/kth-largest-element-in-an-array.py
@@ -7,15 +7,14 @@ import heapq
 
 
 class Solution:
+
     # time: o(nlogn), space: o(1)
-    def findKthLargest(self, nums: List[int], k: int) -> int:
+    def findKthLargest_1(self, nums: List[int], k: int) -> int:
         sorted_nums = sorted(nums)
         return sorted_nums[len(sorted_nums) - k]
 
-
-class Solution2:
     # time= o((n+k)logn) # space: o(n)
-    def findKthLargest(self, nums: List[int], k: int) -> int:
+    def findKthLargest_2(self, nums: List[int], k: int) -> int:
         max_heap = []
         # o(n)
         for num in nums:
@@ -29,8 +28,42 @@ class Solution2:
             kth_largest = heapq.heappop(max_heap)
         return -kth_largest
 
-
-class Solution3:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
+    def findKthLargest_3(self, nums: List[int], k: int) -> int:
         k_largests = heapq.nlargest(k, nums)
         return k_largests[-1]
+
+
+# Solution: QuickSelect
+# Time Complexity:
+#   - Best Case: O(n)
+#   - Average Case: O(n)
+#   - Worst Case: O(n^2)
+# Extra Space Complexity: O(1)
+class Solution2:
+    def partition(self, nums: List[int], left: int, right: int) -> int:
+        pivot, fill = nums[right], left
+
+        for i in range(left, right):
+            if nums[i] <= pivot:
+                nums[fill], nums[i] = nums[i], nums[fill]
+                fill += 1
+
+        nums[fill], nums[right] = nums[right], nums[fill]
+
+        return fill
+
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        k = len(nums) - k
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            pivot = self.partition(nums, left, right)
+
+            if pivot < k:
+                left = pivot + 1
+            elif pivot > k:
+                right = pivot - 1
+            else:
+                break
+
+        return nums[k]
