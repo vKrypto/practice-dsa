0a66c795f9f6ff647cae21d0708ec9594091d782
vkrypto <vkrypto@gmail.com>
2023-06-18 13:42:39 +0530

    auto syncing..

--- dsa/fuck-coding-interviews/problems/daily-tempratures.py
+++ dsa/fuck-coding-interviews/problems/daily-tempratures.py
@@ -25,14 +25,39 @@ class Solution:
         
         return res
 
+    
+    def dailyTemperatures_2(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+
+        stack = [] # [( temp , index)]
+        
+        index = len(temperatures)
+        while index > 0:
+            index -= 1
+            temp = temperatures[index]
+
+            # make sure last item in stack must be greater than cur_item
+            while stack and stack[-1][0] <= temp:
+                stack.pop()
+            
+            # res[index] = cur_index - last_item_index in stack
+            if stack:
+                res[index] = stack[-1][1] - index
+            
+            # append entry in stack
+            stack.append((temp, index))
+        return res
+    
+
     # time: o(n), space=O(n)
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         res = [0] * len(temperatures)
         stack = []  # pair: [temp, index]
 
-        for i, t in enumerate(temperatures):
-            while stack and t > stack[-1][0]:
+        for index, temp in enumerate(temperatures):
+            while stack and temp > stack[-1][0]:
                 stackInd, _ = stack.pop()
-                res[stackInd] = i - stackInd
-            stack.append((i, t))
+                res[stackInd] = index - stackInd
+            stack.append((index, temp))
         return res
+    
