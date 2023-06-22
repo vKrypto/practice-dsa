24bc208b7b2274b5a15af550707c62a2ad6f4fe2
vkrypto <vkrypto@gmail.com>
2023-06-22 23:55:43 +0530

    auto syncing..

--- dsa/fuck-coding-interviews/problems/house-robber.py
+++ dsa/fuck-coding-interviews/problems/house-robber.py
@@ -3,8 +3,8 @@ from typing import List
 
 class Solution:
     
-    # recursive solution: TLE
-    def rob(self, nums: List[int]) -> int:
+    # recursive solution
+    def rob_recursive(self, nums: List[int]) -> int:
         if len(nums) < 2:
             return max(nums)
 
@@ -20,6 +20,23 @@ class Solution:
             return dp[index]
 
         return max(dfs(0), dfs(1))
+    
+    # iterative solution
+    def rob_iterative(self, nums: List[int]) -> int:
+        dp = [0]* len(nums) + [0]*3
+
+        for i in range(len(nums)-1, -1, -1):
+            dp[i] = nums[i] +  max(dp[i+2], dp[i+3])
+
+        return max(dp[0], dp[1])
+    
+    # iterative memory optimized solution
+    def rob_iterative_optimized(self, nums: List[int]) -> int:
+        a, b, c = 0, 0, 0
+        for i in range(len(nums)):
+            a, b, c = nums[i] +  max(b, c), a, b
+        return max(a, b)
+    
     
     def rob(self, nums: List[int]) -> int:
         rob1, rob2 = 0, 0
