ae741a9193a897b99009465c5e5078ed45754c89
vkrypto <vkrypto@gmail.com>
2023-08-02 19:24:50 +0530

    auto syncing..

--- self_practice/gfg/graph/graph_coloring.py
+++ self_practice/gfg/graph/graph_coloring.py
@@ -2,15 +2,42 @@ from collections import defaultdict
 from typing import List
 
 
-class Graph:
+class Graph():
 
     def __init__(self, vertices):
         self.V = vertices
-        self.graph = defaultdict(set)
+        self.graph = defaultdict(list)
+        self.colors = [-1] * self.V
         
     def addEdge(self, a, b):
-        self.graph[a].add(b)
-        self.graph[b].add(a)
+        self.graph[a].append(b)
+        self.graph[b].append(a)
+
+    def isSafe(self, v, c):
+        for child in self.graph[v]:
+            if self.colors[child] == c:
+                return False
+        return True
+
+    def _graphColorUtil(self, m, v=0):
+        for c in range(m):
+            # check if safe decision
+            if self.isSafe(v, c):
+                # step 1: add a step
+                self.colors[v] = c 
+                # step 2: go deeper
+                if  v == self.V-1 or self._graphColorUtil(m, v + 1):
+                    return True
+                # step 3: backtrack if wrong decision
+                self.colors[v] = 0
+
+    def graphColoring_backtracking(self, m=0):
+        if self._graphColorUtil(m, 0) is None:
+            print("Backtrack Solution does not exist")
+            return False
+        else:
+            print("assigned color using Backtrack: ", self.colors)
+            return True
 
     def _get_nodes_sort_by_degree(self) -> List:
         res = []
@@ -19,8 +46,7 @@ class Graph:
         res.sort()    
         return res
 
-
-    def graphColoring(self, m=None) -> None:
+    def graphColoring_greedy(self, m=None) -> None:
         m = m or self.V
 
         colors = [-1]* self.V
@@ -53,9 +79,7 @@ class Graph:
                 if colors[child] != -1:
                     available_colors[colors[child]] = True
 
-        print("assigned color: ", colors)
-                
-
+        print("assigned color using    greedy: ", colors)
 
 # Driver Code
 if __name__ == '__main__':
@@ -68,5 +92,6 @@ if __name__ == '__main__':
     g2.addEdge(4, 3)
 
     # Function call
-    g2.graphColoring(3)
+    g2.graphColoring_backtracking(3)
+    g2.graphColoring_greedy(3)
 
