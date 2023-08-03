2664f40b21e0c907216d9cfe276268af10d653d7
vkrypto <vkrypto@gmail.com>
2023-08-04 01:16:48 +0530

    auto syncing..

--- dsa/fuck-coding-interviews/problems/number-of-islands.py
+++ dsa/fuck-coding-interviews/problems/number-of-islands.py
@@ -2,48 +2,35 @@
 
 from typing import List
 
+
 class Solution:
 
     def numIslands(self, grid: List[List[str]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
         col, row = len(grid), len(grid[0])
         visited = set()
         
-        # def dfs(i, j):
-        #     try:
-        #         if grid[i][j] == '0' or (i,j) in visited:
-        #             return
-        #     except:
-        #         return
 
-        #     visited.add((i, j))   
-        #     dfs(i-1, j)  # check left
-        #     dfs(i+1, j)  # check right 
-        #     dfs(i, j-1)  # check up
-        #     dfs(i, j+1)  # check down
-        
-        dir = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-
-        def helper(c: int, r: int):
+        def dfs(c: int, r: int):
+            # this statement should not be required.
+            # if not(0 <= c < col and 0 <= r < row and (c,r)):
+            #     return
             try:
-                if grid[c][r] == '0':
+                if grid[c][r] == '0' or (c,r) in visited:
                     return
-            except:
+            except IndexError:
                 return
-
             visited.add((c, r))
 
-            for i, j in dir:
-                nc, nr = c+i, r+j
-                if 0 <= nc < col and 0 <= nr < row and (nc, nr) not in visited:
-                    helper(nc, nr)
-
+            dfs(c-1, r)  # check left
+            dfs(c+1, r)  # check right 
+            dfs(c, r-1)  # check up
+            dfs(c, r+1)  # check down
+        
         res = 0
-        for i in range(m):
-            for j in range(n):
+        for i in range(col):
+            for j in range(row):
                 if (i,j) not in visited and grid[i][j] == '1':
-                    helper(i, j)
+                    dfs(i, j)
                     res += 1
         return res
 
