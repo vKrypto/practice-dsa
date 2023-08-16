3808aee248a1868fbee2967e4a3b269fb16f4d41
vkrypto <vkrypto@gmail.com>
2023-08-17 02:35:57 +0530

    auto syncing..

---
+++ dsa/fuck-coding-interviews/problems/ninja-s-training.py
@@ -0,0 +1,25 @@
+# https://www.codingninjas.com/studio/problems/ninja-s-training_3621003
+
+from typing import List
+from collections import defaultdict
+
+  
+def ninjaTraining(n: int, points: List[List[int]]) -> int:
+
+    memo = defaultdict(list)
+    # Write your code here.
+    def rec(index, last_task=None):
+        if index < 0:
+            return 0
+        if (index, last_task) in memo:
+            return memo[(index, last_task)]
+
+        max_points = 0
+        for task_index, task_point in enumerate(points[index]):
+            if task_index != last_task:
+                max_points = max(max_points, task_point + rec(index-1, task_index))
+        
+        memo[(index, last_task)] = max_points
+        return max_points
+
+    return rec(len(points)-1)
