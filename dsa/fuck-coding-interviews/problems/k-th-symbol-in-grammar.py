519c9effb4b2f7e99bb97607c116a8f6ef9e2114
vkrypto <vkrypto@gmail.com>
2023-08-20 12:29:56 +0530

    auto syncing..

--- dsa/fuck-coding-interviews/problems/k-th-symbol-in-grammar.py
+++ dsa/fuck-coding-interviews/problems/k-th-symbol-in-grammar.py
@@ -15,6 +15,7 @@ class Solution:
         row_n = row_data(n) 
         return int(row_n[k-1])
     
+    # time o(k*2)
     def kthGrammar_iterative(self, n: int, k: int) -> int:
         
         prev_row = "0"
@@ -29,9 +30,22 @@ class Solution:
 
         return int(prev_row[k-1])
 
+    # time: o(n), space: stack(o(n)
     def kthGrammar_optimized(self, n: int, k: int) -> int:
         def row_data(row_index, col_index):
             if row_index == 0:
+                return '0'
+            
+            prev_row = row_data(row_index-1, col_index//2)
+            res = "01" if prev_row == '0' else "10"
+            return res[col_index % 2]
+
+        return int(row_data(n-1, k-1) )
+    
+    # time: o(min(n, k)), space: stack(o(min(n, k))
+    def kthGrammar(self, n: int, k: int) -> int:
+        def row_data(row_index, col_index):
+            if row_index == 0 or col_index==0:
                 return '0'
             
             prev_row = row_data(row_index-1, col_index//2)
