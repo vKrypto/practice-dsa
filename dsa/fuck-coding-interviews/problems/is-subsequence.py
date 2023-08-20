6c3b0fd8f3808aff38450fa82e1b45d95041f55e
Ashutosh55 <vkrypto@gmail.com>
2023-08-21 02:05:07 +0530

    auto syncing with msg-''

--- dsa/fuck-coding-interviews/problems/is-subsequence.py
+++ dsa/fuck-coding-interviews/problems/is-subsequence.py
@@ -5,6 +5,16 @@ https://leetcode.com/problems/is-subsequence/
 
 
 class Solution:
+    
+    def isSubsequence(self, t: str, s: str) -> bool:
+        i = j = 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                j += 1
+            i += 1
+        return len(t) == j
+
+
     def isSubsequence(self, s: str, t: str) -> bool:
         if not s:
             return True
