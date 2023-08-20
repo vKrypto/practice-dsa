ad47ceb8d1221de2c33927615f3c1f850b8ae8fa
Ashutosh55 <vkrypto@gmail.com>
2023-08-21 02:02:18 +0530

    auto syncing with msg-''

---
+++ self_practice/test.py
@@ -0,0 +1,16 @@
+# def print_num(*args, **kwargs):
+#     print(args, ">>>", kwargs)
+
+
+# print_num('a', 'b')  # *args
+# print_num('b', 'a')  # *args
+# print_num('b')  # *args
+
+
+# print_num(b='b', a='a', c=4) # **kwargs
+
+
+l = [10, 20, 30, 40, 50, 60, 70, 80]
+g = [1, 2, 3, 4, 5, 6, 7]
+
+print(l , g, end='\n', sep=', ')
