44748b704cf8b6281715787d7c29c7b5602fc7ef
vkrypto <vkrypto@gmail.com>
2023-08-26 22:25:15 +0530

    auto syncing..

--- dsa/fuck-coding-interviews/problems/merge-intervals.py
+++ dsa/fuck-coding-interviews/problems/merge-intervals.py
@@ -27,3 +27,18 @@ class Solution:
                 last_merged_pair[1] = pair[1]
 
         return output
+ 
+    # time: o(n), space: o(n)
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda pair: pair[0])
+        output = [intervals[0]]
+
+        for start, end in intervals:
+            lastEnd = output[-1][1]
+
+            if start <= lastEnd:
+                # merge
+                output[-1][1] = max(lastEnd, end)
+            else:
+                output.append([start, end])
+        return output
