5f471d0807da60cb2b845be1ebad3317728bea9c
vkrypto <vkrypto@gmail.com>
2023-08-28 11:12:59 +0530

    auto syncing..

--- dsa/fuck-coding-interviews/problems/edit-distance.py
+++ dsa/fuck-coding-interviews/problems/edit-distance.py
@@ -45,3 +45,21 @@ class Solution:
                 else:
                     dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j + 1], dp[i + 1][j + 1])
         return dp[0][0]
+
+    # space optimization on tabular method
+    def minDistance(self, word1: str, word2: str) -> int:
+        m,n = len(word1),len(word2)
+        prev=[0]*(n+1)
+        curr=[0]*(n+1)
+        for j in range(n+1):
+            prev[j]=j
+        for i in range(1,m+1):
+            curr[0]=i
+            for j in range(1,n+1):
+                if word1[i-1] == word2[j-1]:
+                    curr[j] = prev[j-1]
+                else:
+                    curr[j] = 1 + min(prev[j-1], prev[j], curr[j-1])
+            prev=curr[:]
+        return prev[n]
+    
