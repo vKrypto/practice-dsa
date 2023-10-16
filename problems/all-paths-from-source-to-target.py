c0f32b553dd53bf720ec7989eaf9b6a922ce43be
vkrypto <vkrypto@gmail.com>
2023-10-16 23:03:38 +0530

    auto syncing..

--- problems/all-paths-from-source-to-target.py
+++ problems/all-paths-from-source-to-target.py
@@ -5,6 +5,7 @@ from typinf import List
 
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        # ignore cyclic paths as questions  SAYS it's acyclic DAG
         paths = []
 
         n = len(graph)
@@ -24,20 +25,41 @@ class Solution:
 
         return paths
 
-    # 60% beats in memory,  88.43% beats in performance
-    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        paths = []
+
+    # backtrack - method
+    def allPathsSourceTarget_1(self, graph: List[List[int]]) -> List[List[int]]:
         n = len(graph)
+        source = 0
+        target = n-1
+        res = []
+        path = []
+        def dfs(node=source):
+            if node == target:
+                res.append([0] + path.copy())
+                return
+            
+            for child_node in graph[node]:
+                path.append(child_node)
+                dfs(child_node)
+                path.pop()
 
-        def dfs(node=0, parents=[0]):
-            if node == n-1:
-                paths.append(parents)
-            else:
-                for child in graph[node]:
-                    dfs(child, parents + [child])
-        
         dfs()
+        return res
 
-        return paths
-                
-                
+
+    # recursion - method
+    def allPathsSourceTarget_2(self, graph: List[List[int]]) -> List[List[int]]:
+        n = len(graph)
+        source, target = 0, n-1
+        res = []
+
+        def dfs(node=source, path=[0]):
+            if node == target:
+                res.append(path)
+                return
+            
+            for child_node in graph[node]:
+                dfs(child_node, path + [child_node])
+
+        dfs()
+        return res
