5d7abaaebe81ff9f351fe12572c8fa15bd461612
vkrypto <vkrypto@gmail.com>
2023-10-22 20:15:22 +0530

    auto syncing..

--- problems/find-center-of-star-graph.py
+++ problems/find-center-of-star-graph.py
@@ -3,8 +3,15 @@
 from collections import defaultdict
 from typinf import List
 
+    
 class Solution:
-    def findCenter(self, edges: List[List[int]]) -> int:
+
+    # edge case-missing (when center is 3 points away from edge)
+    def findCenter_1(self, e: List[List[int]]) -> int:    
+        return e[0][e[0][1] in e[1]]
+
+    # edge case-missing (when center is 3 points away from edge)
+    def findCenter_2(self, edges: List[List[int]]) -> int:
         graph = defaultdict(list)
         for u,v in edges:
             if graph[v]:
@@ -13,3 +20,27 @@ class Solution:
                 return u
             graph[u].append(v)
             graph[v].append(u)
+            
+        
+    def findCenter(self, edges: List[List[int]]) -> int:
+        self.graph = defaultdict(list)
+
+        for (a, b) in edges:
+            self.graph[a].append(b)
+            self.graph[b].append(a)
+
+        def dfs(node=1, path=[0]):
+            # print("path", path)
+            path.append(node)
+            for child in self.graph[node]:
+                if child == path[-2]:
+                    continue
+                else:
+                    path = dfs(child, path)
+                    break
+            
+            return path
+        
+        path = dfs()
+        return path[len(path)//2]
+
