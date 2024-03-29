11af6fe7bf42d247c52dba5242ecc47d9b2f3327
Ashutosh55 <ashutosh.verma@ornaz.com>
2024-03-30 00:00:29 +0530

    auto syncing

--- problems/maximum-product-subarray.py
+++ problems/maximum-product-subarray.py
@@ -1,3 +1,5 @@
+from typing import List
+
 '''Leetcode - https://leetcode.com/problems/maximum-product-subarray/'''
 
 '''
@@ -12,16 +14,6 @@ Output: 6
 '''
 
 # Solution1
-def maxProduct(nums):
-    if(len(nums)) == 0:
-        return 0
-    maxp = nums[0]
-    for i in range(len(nums)):
-        currp = 1
-        for j in range(i, len(nums)):
-            currp *= nums[j]
-            maxp = max(maxp, currp)
-    return maxp
 
 # T:O(N^2)
 # S:O(1)
@@ -44,3 +36,62 @@ def maxProduct(nums):
 
 # T:O(N)
 # S:O(1)
+
+
+class Solution:
+    def maxProduct(nums):
+        """
+        brute-force solution
+        """
+        if(len(nums)) == 0:
+            return 0
+        maxp = nums[0]
+        for i in range(len(nums)):
+            currp = 1
+            for j in range(i, len(nums)):
+                currp *= nums[j]
+                maxp = max(maxp, currp)
+        return maxp
+
+
+    def maxProduct(self, nums: List[int]) -> int:
+        """
+        iterative solution
+        """
+        global_max = max(nums)
+
+        local_max = 1
+        local_min = 1
+        for i in nums:
+            if i ==0:
+                local_max = 1
+                local_min = 1
+                continue
+            min_temp = max(local_min * i, local_max * i)
+            max_temp = min(local_min * i, local_max * i)
+
+            local_max = max(max_temp, min_temp, i)
+            local_min = min(max_temp, min_temp, i)
+
+            global_max = max(local_max, global_max)
+        return global_max
+
+    def maxProduct(self, nums: List[int]) -> int:
+        """
+        recursive solution
+        """
+        g_max = [float('-inf')]
+        def rec(index):
+            if index > len(nums)-1:
+                return 1, 1
+            i = nums[index]
+            _max, _min = rec(index+1)
+            if i==0:
+                res = 0, 0
+            else:
+                res = max(_max*i, _min*i, i), min(_max*i, _min*i, i)
+            g_max[0] = max(g_max[0], *res)
+            return res
+        
+        rec(0)
+        return g_max[0]
