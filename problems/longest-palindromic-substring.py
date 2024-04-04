6327c3c2377dcec9a8ddafb3575e465678f09fdb
ashutosh <vkrypto@gmail.com>
2024-04-05 01:24:46 +0530

    auto syncing..

--- problems/longest-palindromic-substring.py
+++ problems/longest-palindromic-substring.py
@@ -3,7 +3,7 @@
 class Solution:
     
     # recursive solution
-    def longestPalindrome(self, s: str) -> str:
+    def longestPalindrome_1(self, s: str) -> str:
         res = [s[0], 1] # char, count
         n = len(s)
 
@@ -26,3 +26,29 @@ class Solution:
             rec(i-1, i+1) # even length case.
 
         return res[0]
+
+    # iterate solution
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+        resLen = 0
+
+        for i in range(len(s)):
+            # odd length
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l : r + 1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+
+            # even length
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l : r + 1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+
+        return res
