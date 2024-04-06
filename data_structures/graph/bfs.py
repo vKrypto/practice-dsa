4cf24a0da753fb8682c3cf906ed725ede870a39e
ashutosh <vkrypto@gmail.com>
2024-04-06 23:30:42 +0530

    auto syncing..

--- data_structures/graph/bfs.py
+++ data_structures/graph/bfs.py
@@ -4,6 +4,7 @@ class Graph:
         self.node_count = node_count
         self.graph =[[]] 
         
+    #  iterative bfs
     def bfs(self, source=0):
         visited = set()
         path = []
@@ -20,6 +21,38 @@ class Graph:
                         queue.append(neighbor_node)
         print(path)
 
+    # recursive solution
+    def bfs_recursive(self, source=0):
+        visited = set()
+        path = []
+        levels = [[source]]
+        
+        def rec(cur_level, level=0):
+            next_level = []
+            while cur_level:
+                cur_node = cur_level.pop(0)
+                if cur_node in visited:
+                    continue
+                visited.add(cur_node)
+                path.append(cur_node)
+                for neighbor_node, neighbor_node_dist  in enumerate(self.graph[cur_node]):
+                    if neighbor_node_dist == 0:
+                        continue
+                    if neighbor_node not in visited:
+                        next_level.append(neighbor_node)
+            if next_level:
+                levels.append(next_level.copy())
+                rec(next_level, level+1)
+        
+        rec([source])
+        print(path)
+        self.__print_recursion_tree(levels)
+    
+    def __print_recursion_tree(self, levels):
+        print("-----level wise traversal-----")
+        for i, level in enumerate(levels):
+            print(f"level: {i}", level)            
+
 # Driver's code
 if __name__ == '__main__':
     g = Graph(9)
@@ -33,6 +66,8 @@ if __name__ == '__main__':
                [8, 11, 0, 0, 0, 0, 1, 0, 7],
                [0, 0, 2, 0, 0, 0, 6, 7, 0]
                ]
-
+    print("iterative bfs")
     g.bfs()
+    print("recursive bfs")
+    g.bfs_recursive()
 
