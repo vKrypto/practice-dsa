6588cac6f0c73867825f8d1914308153895e4539
ashutosh <vkrypto@gmail.com>
2024-06-13 19:47:34 +0530

    auto syncing..

--- problems/permutations.py
+++ problems/permutations.py
@@ -11,8 +11,7 @@ class Solution:
         return itertools.permutations(nums)
 
 
-class Solution2:
-    def permute(self, nums: List[int]) -> List[List[int]]:
+    def permute_1(self, nums: List[int]) -> List[List[int]]:
         def permutations(elements):
             if len(elements) <= 1:
                 yield elements  # elements is a list.
@@ -23,3 +22,43 @@ class Solution2:
                         yield [first, ] + permutation
 
         return permutations(nums)
+
+    # nums: variable to backtrack
+    def permute_2(self, nums: List[int]) -> List[List[int]]:
+        stack = []
+        visited = [False] * len(nums)
+        res=[]
+        def backtrack(i=0):
+            if len(stack) == len(nums):
+                res.append(stack[::])
+                return
+            
+            for j, num in enumerate(nums):
+                if not visited[j]:
+                    visited[j] = True
+                    stack.append(num)
+                    backtrack(i+1)
+                    stack.pop()
+                    visited[j] = False
+
+        backtrack(0)
+        return res
+
+    # nums: variable to backtrack
+    def permute_3(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        # base-case
+        if len(nums) == 1:
+            return [[nums[0]]]
+        # do all the stuffs, explore all possible cases( split branch)
+        for i in range(len(nums)):
+            num = nums.pop(0) # pop one item
+            res_i = self.permute(nums)
+            nums.append(num) # add back
+
+            # combine result to return.
+            for res_index in range(len(res_i)):
+                res_i[res_index].append(num)
+            res.extend(res_i)
+
+        return res
--- self_practice/heccoach.py
+++ self_practice/heccoach.py
@@ -21,3 +21,48 @@ def is_prime(n):
 # print(is_prime(15))  # False
 
 print(is_prime(901))
+
+
+"""
+1 -> n
+for i in range(n)
+1 -> sqrt(n)
+for i in range(1, sqrt(n))
+1 -> sqrt(n) (ignore even numbers)
+for i in range(1, sqrt(n), 2)
+
+5,_,7,_,_
+1 -> sqrt(n) (ignore even numbers,and block size = 6)
+for i in range(1, sqrt(n), 6):
+    if n % i == 0 or n % (i + 2) == 0:
+        return False
+
+time complexity : omega(sqrt(n)/6), o(sqrt(n))
+ 
+"""
+
+# import pdb
+
+# def add(a, b):
+#     pdb.set_trace()
+#     return a + b
+
+# print(add(3, 4))
+from functools import wraps
+
+def repeat(n):
+    
+    @wraps
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            for _ in range(n):
+                result = func(*args, **kwargs)
+            return result
+        return wrapper
+    return decorator
+
+@repeat(3)
+def say_hello(a, b):
+    print("Hello!", a, b)
+
+say_hello(3,4)
